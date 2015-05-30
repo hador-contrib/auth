126b5e6685a92063d5bd1c2aa38cc80be69487c7
Xuyuan Pang <xuyuanp@gmail.com>
2015-05-30 16:05:25 +0800

    Added Auth Filter and testcases

---
+++ auth.go
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2015 Xuyuan Pang
+ * Author: Xuyuan Pang
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package auth
+
+import (
+	"net/http"
+
+	"github.com/Xuyuanp/hador"
+)
+
+// ErrorKey is the key of unauthorized error in context
+const ErrorKey = "github.com/hador-contrib/auth.Error"
+
+// Func is a function to check authorized
+type Func func(*http.Request) error
+
+func (f Func) Auth(req *http.Request) error {
+	return f(req)
+}
+
+// Filter filters request by AuthFunc
+func Filter(f Func) hador.FilterFunc {
+	return func(ctx *hador.Context, next hador.Handler) {
+		err := f.Auth(ctx.Request)
+		if err != nil {
+			ctx.OnError(http.StatusUnauthorized, err)
+			return
+		}
+		next.Serve(ctx)
+	}
+}
---
+++ auth_test.go
@@ -0,0 +1,80 @@
+/*
+ * Copyright 2015 Xuyuan Pang
+ * Author: Xuyuan Pang
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package auth
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/Xuyuanp/hador"
+	"github.com/smartystreets/goconvey/convey"
+)
+
+func TestAuth(t *testing.T) {
+	convey.Convey("Test Auth", t, func() {
+		f := func(req *http.Request) error {
+			if req.Header.Get("Authorization") != "" {
+				return nil
+			}
+			return errors.New("Not Authorized")
+		}
+
+		h := hador.New()
+		h.AddFilters(
+			hador.FilterFunc(func(ctx *hador.Context, next hador.Handler) {
+				ctx.SetErrorHandler(http.StatusUnauthorized, func(args ...interface{}) {
+					text := http.StatusText(http.StatusUnauthorized)
+					if len(args) > 0 {
+						text = fmt.Sprintf("%v", args[0])
+					}
+					http.Error(ctx.Response, text, http.StatusUnauthorized)
+				})
+				next.Serve(ctx)
+			}),
+			Filter(f),
+		)
+
+		h.Get("/test", hador.HandlerFunc(func(ctx *hador.Context) {
+			ctx.Response.Write([]byte("OK"))
+		}))
+
+		convey.Convey("no Authorization", func() {
+			req, _ := http.NewRequest("GET", "/test", nil)
+			resp := httptest.NewRecorder()
+
+			h.ServeHTTP(resp, req)
+
+			convey.So(resp.Code, convey.ShouldEqual, http.StatusUnauthorized)
+			convey.So(resp.Body.String(), convey.ShouldEqual, "Not Authorized\n")
+		})
+
+		convey.Convey("Authorization", func() {
+			req, _ := http.NewRequest("GET", "/test", nil)
+			req.Header.Set("Authorization", "OK")
+			resp := httptest.NewRecorder()
+
+			h.ServeHTTP(resp, req)
+
+			convey.So(resp.Code, convey.ShouldEqual, http.StatusOK)
+			convey.So(resp.Body.String(), convey.ShouldEqual, "OK")
+		})
+	})
+}
